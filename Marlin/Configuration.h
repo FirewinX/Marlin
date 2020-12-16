6931c29e1059d5070604886bab1c274a0e14741e
FirewinX <75076307+FirewinX@users.noreply.github.com>
2020-12-16 13:17:02 +0100

    restore config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1289,13 +1289,13 @@
  * these options to restore the prior leveling state or to always enable
  * leveling immediately after G28.
  */
-#define RESTORE_LEVELING_AFTER_G28
+//#define RESTORE_LEVELING_AFTER_G28
 //#define ENABLE_LEVELING_AFTER_G28
 
 /**
  * Auto-leveling needs preheating
  */
-#define PREHEAT_BEFORE_LEVELING
+//#define PREHEAT_BEFORE_LEVELING
 #if ENABLED(PREHEAT_BEFORE_LEVELING)
   #define LEVELING_NOZZLE_TEMP 120
   #define LEVELING_BED_TEMP     50
