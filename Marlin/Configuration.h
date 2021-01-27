73b53526de08a612c7cf67b6861f8b56fa8fc05c
FirewinX <75076307+FirewinX@users.noreply.github.com>
2021-01-27 14:05:30 +0100

    update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -737,7 +737,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 141 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 81.62, 78.97, 401.00, 142.41 }
 
 /**
  * Default Max Feed Rate (mm/s)
